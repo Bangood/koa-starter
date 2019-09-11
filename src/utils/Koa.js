81d0b5f42df8fcc4ba1a3385ac62f08f9d68e229
bangood <2419281635@qq.com>
2019-09-11 10:28:10 +0800

    添加koa-bodyparser中间件解析post请求的body

--- src/utils/Koa.js
+++ src/utils/Koa.js
@@ -1,12 +1,11 @@
 import koa from 'koa';
+import bodyParser from 'koa-bodyparser';
 import Logger from '../utils/Logger'
 export default class Koa {
     app = new koa();
     constructor(port) {
-        this.app.use(ctx => {
-            ctx.body = 'hello koa starter';
-        });
-        this.app.listen(port,()=>{
+        this.app.use(bodyParser());
+        this.app.listen(port, () => {
             Logger.info(`http server listen on:${port}`)
         });
     }
